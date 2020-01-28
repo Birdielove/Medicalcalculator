f92d2d7aeee7dc9a70bae9843c5bc65b142f05c0
300310436 <lovepreetsinghbirdi@gmail.com>
2020-01-28 10:31:32 -0800

    partialy done

--- app/src/main/java/com/Love/medicalcalculator/MainActivity.java
+++ app/src/main/java/com/Love/medicalcalculator/MainActivity.java
@@ -3,12 +3,42 @@ package com.Love.medicalcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    double value;
+    double result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button calculate = findViewById(R.id.button);
+        final EditText userValue = findViewById(R.id.editText);
+        final RadioButton toKilo = findViewById(R.id.radioButton4);
+        final RadioButton toPound = findViewById(R.id.radioButton3);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (toPound.isChecked()) {
+                    value = Double.parseDouble(String.valueOf(userValue.getText()));
+                    result = value * 2.20462;
+                    Toast.makeText(MainActivity.this, String.valueOf(result), Toast.LENGTH_SHORT).show();
+                }
+                else if(toKilo.isChecked()){
+                    value = Double.parseDouble(String.valueOf(userValue.getText()));
+                    result = value * 0.4535918;
+                    Toast.makeText(MainActivity.this, String.valueOf(result), Toast.LENGTH_SHORT).show();
+                }
+                else if(!(toKilo.isChecked() && toPound.isChecked())){
+                    Toast.makeText(MainActivity.this, "Please check the type first!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
